c7783c2d004af43674db32fca2482c56d4abf34d
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-09-14 21:14:29 +0300

    update-code

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.use(cors());
 app.use(express.json());
 const { response } = require('express');
 
-mongs.connect(procces.env.MONGO);
+const M = `mongodb://MongoMGIATPK:clxanPYKtOojjkm8@ac-ft60bmc-shard-00-00.ir1rb6u.mongodb.net:27017,ac-ft60bmc-shard-00-01.ir1rb6u.mongodb.net:27017,ac-ft60bmc-shard-00-02.ir1rb6u.mongodb.net:27017/?ssl=true&replicaSet=atlas-12rc7j-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-const PORT = process.env.PORT;
+//mongs.connect('mongodb://localhost:27017/book')
+
+mongs.connect(`${M}`);
+
+const PORT = process.env.PORT || 3000;
 
 //http://localhost:3001/test
 app.get('/test.', (request, response) => {
