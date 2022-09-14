293bdae49cb2e46cd03e9a09d63c3908c161b5d2
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-09-14 20:55:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,147 +1,156 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongs = require('mongoose');
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 const { response } = require('express');
 
-// const M = `mongodb+srv://MongoMGIATPK:oCvS3zcKjDTGyw4Q@cluster0.ir1rb6u.mongodb.net/?retryWrites=true&w=majority`;
+const M = `mongodb://books:Ashar121299@ac-ogxaou4-shard-00-00.vnq50b2.mongodb.net:27017,ac-ogxaou4-shard-00-01.vnq50b2.mongodb.net:27017,ac-ogxaou4-shard-00-02.vnq50b2.mongodb.net:27017/?ssl=true&replicaSet=atlas-103vtc-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-// mongs.connect(`${M}`);
+//mongs.connect('mongodb://localhost:27017/book')
 
-const PORT = 3000;
+mongs.connect(`${M}`);
 
-app.get('/test', (request, response) => {
+const PORT = process.env.PORT || 3000;
+
+//http://localhost:3001/test
+app.get('/test.', (request, response) => {
   response.send('test request received')
 })
 
-// const MovieShema = new mongs.Schema({
-//   title: String,
-//   description: String,
-//   status: String,
-//   name: String
-// })
+const BookSchema = new mongs.Schema({
+  title: String,
+  discription: String,
+  status: String,
+  name: String
+})
 
-// const Movie = mongs.model('MovieModel', MovieShema)
+const Book = mongs.model('BookModel', BookSchema)
 
-// async function seedData () {
-//   const First_Movie = new Movie({
-//     title: 'Movie 1',
-//     description: 'Movie 1 Desc',
-//     status: 'Movie 1 In Cinema',
-//     name: "Admin"
-//   })
-//   const Second_Movie = new Movie({
-//     title: 'Movie 1',
-//     description: 'Movie 1 Desc',
-//     status: 'Movie 1 In Cinema',
-//     name: "Admin"
-//   })
-//   const Third_Movie = new Movie({
-//     title: 'Movie 1',
-//     description: 'Movie 1 Desc',
-//     status: 'Movie 1 In Cinema',
-//     name: "Admin"
-//   })
-//   await First_Movie.save()
-//   await Second_Movie.save()
-//   await Third_Movie.save()
-// }
+async function seedData () {
+  const firstBook = new Book({
+    title: 'anatomy',
+    discription: 'Medical subjects',
+    status: 'available',
+    name: "Admin",
+  })
+  const secondBook = new Book({
+    title: 'liguastics',
+    discription: 'English language subjects',
+    status: 'deserved',
+    name: "Admin",
+  })
+  const thirdBook = new Book({
+    title: 'special subject in computer engineer',
+    discription: 'computer language subjects',
+    status: 'not exist',
+    name: "Admin",
+  })
+  await firstBook.save()
+  await secondBook.save()
+  await thirdBook.save()
+}
 
-// seedData();
+//seedData();
 
-// app.get('/Movie', getbookHandler)
+//http://localhost:3001/book
+app.get('/book', getbookHandler)
 
-// function getbookHandler (req, res) {
-//   const name = req.query.name;
-//   Movie.find( {name:name}, (err, result) => {
-//     if (err) {
-//       console.log(err)
-//     } 
-//     else {
-//       res.json(result)
-//     }
-//   }
-//   )
-// }
-
-
-// app.post('/Movie', addHandler);
-
-// async function addHandler(req,res) {
-//   const {title,description,status,name} = req.body;
-//   await Movie.create({
-//     title:title,
-//     description:description,
-//     status:status,
-//     name:name,
-      
-//   });
-
-//   Movie.find({name:name},(err,result)=>{
-//       if(err)
-//       {
-//           console.log(err);
-//       }
-//       else
-//       {
-//           // console.log(result);
-//           res.send(result);
-//       }
-//   })
-// }
+function getbookHandler (req, res) {
+  const name = req.query.name;
+  Book.find( {name:name}, (err, result) => {
+    if (err) {
+      console.log(err)
+    } 
+    else {
+      res.json(result)
+    }
+  }
+  )
+}
 
 
-// app.delete('/Movie/:id',deleteHandler);
- 
-//   function deleteHandler(req,res) { 
-//   const bookId = req.params.id;
-//   const name = req.query.name;
- 
-//   Movie.deleteOne({_id:bookId},(err,result)=>{
-      
-//       Movie.find({name:name},(err,result)=>{ 
-//           if(err)
-//           {
-//             console.log(err);
-//           }
-//           else
-//           {
-             
-//             res.send(result);
-//           }
-//       })
+app.post('/book', addHandler);
 
-//   })
+async function addHandler(req,res) {
+  //console.log(req.body);
   
-// }
+  const {title,discription,status,name} = req.body; //Destructuring assignment
+  await Book.create({
+    title:title,
+    discription:discription,
+    status:status,
+    name:name,
+      
+  });
 
-// app.put('/Movie/:id',updateHandler);
+  Book.find({name:name},(err,result)=>{
+      if(err)
+      {
+          console.log(err);
+      }
+      else
+      {
+          // console.log(result);
+          res.send(result);
+      }
+  })
+}
 
-// function updateHandler(req, res){
-//   const id = req.params.id;
-//   const {title,description,status,name} = req.body;
 
-//   Movie.findByIdAndUpdate(id, {title,description,status,name}, (err, result) => {
-//     if(err){
-//       console.log(err);
-//     } else {
-//       Movie.find({name:name},(err,result)=>{ 
-//         if(err)
-//         {
-//             console.log(err);
-//         }
-//         else
-//         {
-//             res.send(result);
-//         }
-//     })
-//     }
-//   })
+app.delete('/book/:id',deleteHandler);
+ 
+  function deleteHandler(req,res) { 
+  const bookId = req.params.id;
+  const name = req.query.name;
+ 
+  Book.deleteOne({_id:bookId},(err,result)=>{
+      
+      Book.find({name:name},(err,result)=>{ 
+          if(err)
+          {
+            console.log(err);
+          }
+          else
+          {
+             
+            res.send(result);
+          }
+      })
 
-// }
+  })
+  
+}
+
+app.put('/book/:id',updateHandler);
+
+function updateHandler(req, res){
+  const id = req.params.id;
+  const {title,discription,status,name} = req.body;
+
+  Book.findByIdAndUpdate(id, {title,discription,status,name}, (err, result) => {
+    if(err){
+      console.log(err);
+    } else {
+      Book.find({name:name},(err,result)=>{ 
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.send(result);
+        }
+    })
+    }
+  })
+
+}
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
